29a6cdd1791d284256382226f7de0b73f8ac2ca1
Javi <javier.marengo@devbase.us>
2024-01-04 17:21:01 -0300

    feat: innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import aaxisLogo from "/aaxis-logo.webp";
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <img src={aaxisLogo} loading="lazy" />
+    </>
+  )
+}
+
+export default App
