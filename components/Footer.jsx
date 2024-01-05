866d9db2d65126ebcc6916ec013f711969bdd556
Javi <javier.marengo@devbase.us>
2024-01-05 16:19:29 -0300

    fix: adjust footer and content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,28 @@
-import { Box, Typography } from '@mui/material';
-
+import { Box, Typography, IconButton } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 const Footer = () => (
-  <Box component={"footer"} sx={{
-    py: 2,
-  }}>
+  <Box 
+    component={"footer"} 
+    sx={{
+      py: 2,
+      backgroundColor: (theme) => theme.palette.primary.main
+    }}
+  >
     <Typography variant="h6" align="center" gutterBottom>
-      Footer
+      Conversor de monedas Aaxis
     </Typography>
-    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+    <Box textAlign={"center"}>
+    <IconButton component="a" href='https://www.linkedin.com/in/javiermarengo/' target="_blank" rel="noreferrer">
+      <LinkedInIcon />
+    </IconButton>
+    <IconButton href='https://github.com/javim89' target="_blank" rel="noreferrer">
+      <GitHubIcon />
+    </IconButton>
+    </Box>
+    {/* <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
       Something here to give the footer a purpose!
-    </Typography>
+    </Typography> */}
   </Box>
 )
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import Layout from '../components/Layout.jsx';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import CurrencyConverterSection from '../sections/CurrencyConverterSection.jsx';
 import CurrencyConverterLoading from '../sections/CurrencyConverterLoading.jsx';
 import { Suspense } from 'react';
 function App() {
   return (
     <Layout>
-      <Typography variant="h1" textAlign={"center"} fontWeight={400} gutterBottom sx={{
-        fontSize: "4rem",
-        my: 2
-      }}>
-        Conversor de monedas Aaxis
-      </Typography>
-      <Suspense fallback={<CurrencyConverterLoading />}>
-        <CurrencyConverterSection />
-      </Suspense>
+      <Box display="flex" height={600} >
+        <Box margin={"auto"}>
+          <Typography variant="h1" textAlign={{
+            xs: "left",
+            sm: "center"
+          }} fontWeight={400} gutterBottom fontSize={{
+            xs: "3rem",
+            sm: "4rem"
+          }} sx={{
+            my: 2
+          }}>
+            Conversor de monedas Aaxis
+          </Typography>
+          <Suspense fallback={<CurrencyConverterLoading />}>
+            <CurrencyConverterSection />
+          </Suspense>
+        </Box>
+      </Box>
     </Layout>
   )
 }
